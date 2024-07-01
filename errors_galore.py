84efa8b87608bdf2bb840425af0414a1a11cf8b6
Glenn Langdon <glennlangdon@gmail.com>
2024-07-01 17:36:56 -0500

    add errors_galore.py

---
+++ errors_galore.py
@@ -0,0 +1,47 @@
+# This page has python style errors
+import datetime, date
+
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et. Felis eget velit aliquet sagittis id consectetur.
+def func():
+  x = 1
+   y = 2  
+
+x = ( 1 + 2)
+
+y = 1+ 2
+
+z = 1  +2
+
+foo (1, 2)
+
+def bar(x, y):
+   return x + y
+class AnyClass:
+   pass
+
+
+
+def foobar(x, y):
+   return x * y
+
+def StyleFunction():
+   pass
+
+
+def style_function(Var1, Var2):
+   pass
+
+
+if x == None:
+   pass
+
+
+if x == 1: print("x is 1")
+
+
+dict = {'key1': 'value1', "key2": "value2", 'key3': 2001}
+dict['key1'] = dict ['hello']
+
+
+def sample (arg2, arg3):
+    return arg2 + arg3
